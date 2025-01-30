2e875e564e0fda66543e3897d5405916616b1e4b
rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>
2025-01-31 00:38:35 +0100

    Create blockchain_utils.py
    Signed-off-by: rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>

---
+++ utils/blockchain_utils.py
@@ -0,0 +1,8 @@
+# Hilfsfunktionen für Blockchain-Operationen
+def create_blockchain():
+    # Logik zur Erstellung einer Blockchain
+    return True
+
+def add_block(block):
+    # Logik zum Hinzufügen eines Blocks zur Blockchain
+    return True
