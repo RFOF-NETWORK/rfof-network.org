838d809c7c39bcfdd395db6cbe2c12df743670a5
rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>
2025-01-31 00:19:46 +0100

    Create NEC.sol
    Signed-off-by: rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>

---
+++ contracts/bbc/tokens/NEC.sol
@@ -0,0 +1,21 @@
+// NEC.sol
+pragma solidity ^0.8.0;
+
+contract NEC {
+    string public name = "Token NEC";
+    string public symbol = "NEC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 2000000 * (10 ** uint256(decimals));
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance.");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+}
